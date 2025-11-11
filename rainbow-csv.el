emacs-vs/rainbow-csv

commit 0865b7c0e95ca1f8ce561df55d497d2e90774efc

fix: error when called in an empty buffer/file (#9)

rainbow-csv.el
@@ -117,17 +117,18 @@
   (interactive (list (when current-prefix-arg (read-char "Separator: "))))
   (rainbow-csv--revert-font-lock-keywords)
   (font-lock-mode 1)
-  (let* ((separator (or separator
-                        (csv-guess-separator ; From `csv-guess-set-separator'
-                         (buffer-substring-no-properties
-                          (point-min) (min 8192 (point-max)))
-                         2048)))
-         (fields (save-excursion
-                   (goto-char (point-min))
-                   (csv--collect-fields (line-end-position))))
-         ;; Is the first field quoted?
-         (quote-char (string-match-p (format "^\\([%s]\\).*\\1$" (string-join csv-field-quotes)) (car fields)))
-         (quote-char (and quote-char (substring (car fields) nil 1)))) ; Get the first char
+  (when-let* ((separator (or separator
+                             (csv-guess-separator ; From `csv-guess-set-separator'
+                              (buffer-substring-no-properties
+                               (point-min) (min 8192 (point-max)))
+                              2048)))
+              (fields
+               (save-excursion
+                 (goto-char (point-min))
+                 (seq-filter (lambda (s) (not (string-empty-p s))) (csv--collect-fields (line-end-position)))))
+              ;; Is the first field quoted?
+              (quote-char (string-match-p (format "^\\([%s]\\).*\\1$" (string-join csv-field-quotes)) (car fields)))
+              (quote-char (and quote-char (substring (car fields) nil 1)))) ; Get the first char
     (dotimes (i (length fields))
       (let* ((r (if quote-char
                     (format "^\\(%s[^%s]*%s[%c\n]\\)\\{%d\\}"

commit b1c9190b47cc5a9aed4a37fdc2b3720e47536340

fix: properly colorize non-quoted fields
This is a bug has been introduced accidentally in 0865b7c. Without this fix,
this file:

    One,Two,Three

Will not be colorized, while this one:

    "One","Two","Three"

Will.

rainbow-csv.el
@@ -125,21 +125,21 @@
               (fields
                (save-excursion
                  (goto-char (point-min))
-                 (seq-filter (lambda (s) (not (string-empty-p s))) (csv--collect-fields (line-end-position)))))
-              ;; Is the first field quoted?
-              (quote-char (string-match-p (format "^\\([%s]\\).*\\1$" (string-join csv-field-quotes)) (car fields)))
-              (quote-char (and quote-char (substring (car fields) nil 1)))) ; Get the first char
-    (dotimes (i (length fields))
-      (let* ((r (if quote-char
-                    (format "^\\(%s[^%s]*%s[%c\n]\\)\\{%d\\}"
-                            quote-char quote-char quote-char separator (1+ i))
-                  (format "^\\([^%c\n]*[%c\n]\\)\\{%d\\}"
-                          separator separator (1+ i))))
-             (len (length rainbow-csv-colors))
-             (color (nth (% i len) rainbow-csv-colors)))
-        (setq csv-font-lock-keywords
-              (append csv-font-lock-keywords
-                      `((,r (1 '(face (:foreground ,color)) prepend t))))))))
+                 (seq-filter (lambda (s) (not (string-empty-p s))) (csv--collect-fields (line-end-position))))))
+    ;; Is the first field quoted?
+    (let* ((quote-char (string-match-p (format "^\\([%s]\\).*\\1$" (string-join csv-field-quotes)) (car fields)))
+           (quote-char (and quote-char (substring (car fields) nil 1)))) ; Get the first char
+      (dotimes (i (length fields))
+        (let* ((r (if quote-char
+                      (format "^\\(%s[^%s]*%s[%c\n]\\)\\{%d\\}"
+                              quote-char quote-char quote-char separator (1+ i))
+                    (format "^\\([^%c\n]*[%c\n]\\)\\{%d\\}"
+                            separator separator (1+ i))))
+               (len (length rainbow-csv-colors))
+               (color (nth (% i len) rainbow-csv-colors)))
+          (setq csv-font-lock-keywords
+                (append csv-font-lock-keywords
+                        `((,r (1 '(face (:foreground ,color)) prepend t)))))))))
   (font-lock-refresh-defaults))
 
 